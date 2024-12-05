2aa2905a294862ae5d285e1e7c8b8f023c20d8e6
bhushan <bhushan@bhushans-MacBook-Pro.local>
2024-12-05 16:21:30 +0530

    new file

---
+++ Arun.java
@@ -0,0 +1,19 @@
+package Docsy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Arun {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+		 
+		 driver.findElement(By.xpath("")).click();
+		 
+		 driver.findElement(By.xpath("")).submit();
+		 
+		 driver.findElement(By.xpath("")).sendKeys();
+	}
+
+}
