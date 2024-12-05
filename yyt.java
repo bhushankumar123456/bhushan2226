735dbb5e6a804c1748144e1e9df1066fc9787283
bhushan <bhushan@bhushans-MacBook-Pro.local>
2024-12-05 16:28:44 +0530

    yyt

---
+++ yyt.java
@@ -0,0 +1,14 @@
+package Docsy;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class yyt {
+	public static void main(String[] args) {
+		 WebDriver driver = new ChromeDriver();
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+}
